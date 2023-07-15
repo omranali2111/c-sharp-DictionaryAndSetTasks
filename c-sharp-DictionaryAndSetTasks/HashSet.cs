2ce8b459f61ab83e3584b1525b747f8fdcd263c1
omranali2111 <aljabri1234@hotmail.com>
2023-07-15 22:18:22 +0400

    Task 2: get Unique Elements in array using hashset

--- c-sharp-DictionaryAndSetTasks/HashSet.cs
+++ c-sharp-DictionaryAndSetTasks/HashSet.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace c_sharp_DictionaryAndSetTasks
 {
     internal class HashSet
     {
-       
+        /*
+        Task 2: Unique Elements
+        Write a function that takes an array of integers as input and returns a HashSet containing only the
+        unique elements from the array.Test the function with different input arrays and print the resulting
+        HashSet.
+        */
+        public static HashSet<int> GetUniqueElements(int[] array)
+        {
+            HashSet<int> uniqueElements = new HashSet<int>(array);
+            return uniqueElements;
+        }
 
-      
+
+
 
     }
 }
--- c-sharp-DictionaryAndSetTasks/Program.cs
+++ c-sharp-DictionaryAndSetTasks/Program.cs
@@ -13,7 +13,8 @@ internal class Program
         {
             Console.WriteLine($"{kvp.Key}: {kvp.Value}");
         }
-        */
+        
+
 
         PhoneBook phoneBook = new PhoneBook();
 
@@ -27,6 +28,16 @@ internal class Program
         phoneBook.remove("omran");
 
         phoneBook.search("omran");
+        */
+
+
+        int[] array = { 1, 2, 3, 4, 5, 5, 4, 3, 2, 1 };
+        HashSet<int> uniqueSet1 = HashSet.GetUniqueElements(array);
+        Console.WriteLine("Unique elements in array:");
+        foreach (int element in uniqueSet1)
+        {
+            Console.Write(element+" ");
+        }
 
 
     }
